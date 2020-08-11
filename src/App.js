a1876426a3055ba737c5bd1ad91c514efec9976b
kalaa91 <12202454+kalaa91@users.noreply.github.com>
2020-08-12 00:21:43 +0200

    Updating app entry point component with new routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import "./App.css";
+
+const QuestionsPage = React.lazy(() =>
+  import("./features/ListQuestions/QuestionsPage")
+);
+const VotingPage = React.lazy(() => import("./features/Vote/VotingPage"));
+const AddQuestionPage = React.lazy(() =>
+  import("./features/AddNewQuestion/AddQuestionPage")
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" render={() => <QuestionsPage />} />
+          <Route exact path="/vote/" component={VotingPage} />
+          <Route exact path="/add-new/" component={AddQuestionPage} />
+          <Redirect to="/" />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
